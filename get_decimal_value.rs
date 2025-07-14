1305437037ae47ca1c04c7f5bd7bf81d45170f63
angrymiaucino <77390188+AncutaIoan@users.noreply.github.com>
2025-07-14 19:47:45 +0300

    Create get_decimal_value.rs

---
+++ get_decimal_value.rs
@@ -0,0 +1,40 @@
+/*
+1290. Convert Binary Number in a Linked List to Integer
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+*/
+
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
+        let mut result = 0;
+        let mut current = head;
+
+        while let Some(node) = current {
+            result = (result << 1) | node.val;
+            current = node.next;
+        }
+
+        result
+    }
+}
